6eaed9014f04af9154bc75983e42eb9dbaeda021
Oscar <oscar.llapar@educa.jcyl.es>
2022-01-13 21:58:39 +0100

    13/01/2022: Página de error (sin poder volver)

--- controller/cError.php
+++ controller/cError.php
@@ -1,5 +1,21 @@
 <?php
-//Boton volver
-//Coger el objeto AppError en $_SESSION
+
+
 //Formatear salida
 //Pasar valores a vista en array
+
+    if(isset($_REQUEST['volver'])){
+        $_SESSION['paginaEnCurso'] = $_SESSION['error']->getPaginaSiguiente();
+        header('Location: index.php');
+        exit;
+    };
+    
+    $aError=[
+        "mensaje" => $_SESSION['error']->getMensaje(),
+        "codigo" => $_SESSION['error']->getCodigo()
+    ];
+            
+    $paginaEnCurso = 'error';
+    require_once "view/LayoutHeader.php";
+    require_once $aVistas[$paginaEnCurso];
+    require_once "view/LayoutFooter.php";
